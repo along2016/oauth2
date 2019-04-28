5653fee93c571956dd8e7ca3bbf924d811079584
dtwangjiliang <845506513@qq.com>
2019-04-28 16:58:43 +0800

    去掉无用的数据源

--- servers/ResourceServer/src/main/java/com/aak/ResourceServerApplication.java
+++ servers/ResourceServer/src/main/java/com/aak/ResourceServerApplication.java
@@ -1,21 +1,14 @@
 package com.aak;
 
-import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import javax.sql.DataSource;
 import java.util.Collections;
 
 @SpringBootApplication
@@ -35,12 +28,6 @@ public class ResourceServerApplication {
 		bean.setOrder(0);
 		return bean;
 	}
-
-	@Bean
-	@Primary
-	@ConfigurationProperties(prefix = "spring.datasource")
-	public DataSource mainDataSource() {
-		return DataSourceBuilder.create().type(HikariDataSource.class).build();}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceServerApplication.class, args);
